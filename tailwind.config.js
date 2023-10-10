23a1a29780ae4848686f9d3a1aa6a7e9f3574415
Bartłomiej Cywiński <bartlomiej.cywinski@poczta.onet.pl>
2023-10-10 19:12:58 +0200

    Finished styles

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,12 +5,22 @@ module.exports = {
     extend: {
       maxWidth:{
        '250px' : '250px',
-      },
+       },
+      width:{
+        '500' : '500px',
+       },
+      height:{
+        '300' : '300px',
+       },
       minHeight:{
         '150px' : '150px',
        },
        fontFamily: {
         'IrishG' : ['Irish Grover', 'sans-serif'],
+        'Inter' : ['Inter']
+       },
+       colors:{
+        'bg-custom-gray' : '#949494',
        }
     },
   },
